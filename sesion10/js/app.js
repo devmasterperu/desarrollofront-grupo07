08923b3088593aedd19d0a54ca85403fb8db9dd3
gmanriqe <gmanriqe@gmail.com>
2019-11-09 11:09:44 -0500

    jQuery

--- sesion10/js/app.js
+++ sesion10/js/app.js
@@ -4,4 +4,16 @@ $(document).ready(function(){
 
     $(".caja").prepend("<p>Contenido creado con jQuery - Prepend</p>")
     $(".caja").append("<p>Contenido creado con jQuery - append</p>");
+
+    $(".caja").css("background-color");
+    $(".caja").css(["background-color","border-radius"]);
+    $(".caja").css({
+        "width":"300px",
+        "height":"300px",
+        "background-color":"deeppink"
+    });
+
+    $(".parrafo").remove();
+
+    // 😭 https://api.jquery.com/category/manipulation/
 });
